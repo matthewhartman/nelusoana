6ed9de287b7ff40b7f5d0d1d192f171270aa817f
Matthew Hartman <matthewhartmanau@gmail.com>
2017-08-05 22:49:24 +1000

    Added contact section

--- src/js/default.js
+++ src/js/default.js
@@ -6,3 +6,26 @@ WebFont.load({
     families: ['Source Sans Pro:200,300,400,600,700,900', 'Clicker Script']
   }
 });
+
+// Google Map
+function googleMap() {
+  var map = document.getElementById('google-map');
+
+  var map_options = {
+    center: new google.maps.LatLng(-38.033811,145.202865),
+    scrollwheel: false,
+    zoom: 18,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(map, map_options)
+
+  // Map Marker
+  var myLatlng = new google.maps.LatLng(-38.033811,145.202865);
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    icon: '../img/pin.svg'
+  });
+  map.panBy(-300, 0);
+}
+google.maps.event.addDomListener(window, 'load', googleMap);
